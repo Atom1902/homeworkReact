bd4aa0c0f561c9f56b05ea85cc1892259a6972d5
Atom1902 <atom1902@gmail.com>
2022-09-14 13:09:41 +0300

    hw2Simpson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import RickAndMorty from "./RickAndMorty/RickAndMorty";
+import './RickAndMorty/RickAndAMortyStyle.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='allCharacters'>
+            <RickAndMorty
+                id = '1'
+                name = 'Rick Sanchez'
+                status = "Alive"
+                species = "Human"
+                gender = "Male"
+                image = "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            />
+            <RickAndMorty
+                id = '420'
+                name = 'Plutonian Host'
+                status = 'Alive'
+                species = 'Alien'
+                gender = 'Male'
+                image = 'https://rickandmortyapi.com/api/character/avatar/420.jpeg'
+            />
+            <RickAndMorty
+                id = '123'
+                name = 'Fat Morty'
+                status = 'Alive'
+                species = 'Human'
+                gender = 'Male'
+                image = 'https://rickandmortyapi.com/api/character/avatar/123.jpeg'
+            />
+            <RickAndMorty
+                id = '600'
+                name = 'Cats Fan'
+                status = 'Alive'
+                species = 'Alien'
+                gender = 'Female'
+                image = 'https://rickandmortyapi.com/api/character/avatar/600.jpeg'
+            />
+            <RickAndMorty
+                id = '248'
+                name = 'Mrs. Refrigerator'
+                status = 'Dead'
+                species = 'Alien'
+                gender = 'Female'
+                image = 'https://rickandmortyapi.com/api/character/avatar/248.jpeg'
+            />
+            <RickAndMorty
+                id = '306'
+                name = 'Scary Terry'
+                status = 'Alive'
+                species = 'Mythological Creature'
+                gender = 'Male'
+                image = 'https://rickandmortyapi.com/api/character/avatar/306.jpeg'
+            />
+        </div>
+    )
 }
 
 export default App;
